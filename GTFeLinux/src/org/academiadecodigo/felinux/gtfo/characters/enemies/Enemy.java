e569ce5c72198e43490f62c9ba5ebba35476f8ab
Pikachu <davidmrferreira@hotmail.com>
2020-06-27 13:46:21 +0100

    Enemy movement DONE, ENUM DirectionType created

--- GTFeLinux/src/org/academiadecodigo/felinux/gtfo/characters/enemies/Enemy.java
+++ GTFeLinux/src/org/academiadecodigo/felinux/gtfo/characters/enemies/Enemy.java
@@ -12,6 +12,8 @@ public abstract class Enemy implements Moveable {
     private Field enemyField;
     private boolean isDead = false;
     private Picture enemyImage;
+    private DirectionType directionType = DirectionType.LEFT;
+    private DirectionType lastDirectionType = DirectionType.RIGHT;
 
     public Enemy(Field enemyField, int posX, int posY, String spriteName){
         this.enemyField = enemyField;
@@ -38,37 +40,74 @@ public abstract class Enemy implements Moveable {
 
     @Override
     public void moveLeft() {
+
+        this.directionType = DirectionType.LEFT;
+
+        if(this.lastDirectionType == DirectionType.RIGHT){
+            System.out.println("This Direction is: " + DirectionType.LEFT + " Last Direction was: " + DirectionType.RIGHT);
+            return;
+        }
+
         if(this.enemyField.getPadding()  >= enemyImage.getX()){
             enemyImage.translate(enemyField.getCellSize()*2,0);
             return;
         }
         enemyImage.translate(-enemyField.getCellSize()*2,0);
+
+        this.lastDirectionType = DirectionType.LEFT;
     }
 
     @Override
     public void moveRight() {
+        this.directionType = DirectionType.RIGHT;
+
+        if(this.lastDirectionType == DirectionType.LEFT){
+            System.out.println("This Direction is: " + directionType + " Last Direction was: " + lastDirectionType);
+            return;
+        }
+
         if(this.enemyField.getSizeCol() <= enemyImage.getMaxX() - this.enemyField.getPadding()){
             enemyImage.translate(-enemyField.getCellSize()*2,0);
             return;
         }
         enemyImage.translate(enemyField.getCellSize()*2,0);
+
+        this.lastDirectionType = DirectionType.RIGHT;
     }
 
     @Override
     public void moveUp() {
+        this.directionType = DirectionType.UP;
+
+        if(this.lastDirectionType == DirectionType.DOWN){
+            System.out.println("This Direction is: " + directionType + " Last Direction was: " + lastDirectionType);
+            return;
+        }
+
         if(this.enemyField.getPadding() >= enemyImage.getY()){
             enemyImage.translate(0,enemyField.getCellSize()*2);
             return;
         }
         enemyImage.translate(0,-enemyField.getCellSize()*2);
+
+        this.lastDirectionType = DirectionType.UP;
     }
 
     @Override
     public void moveDown() {
+        this.directionType = DirectionType.DOWN;
+
+        if(this.lastDirectionType == DirectionType.UP){
+            System.out.println("This Direction is: " + directionType + " Last Direction was: " + lastDirectionType);
+            return;
+        }
+
         if(this.enemyField.getSizeRow() <= enemyImage.getMaxY() - this.enemyField.getPadding()){
             enemyImage.translate(0,-enemyField.getCellSize()*2);
             return;
         }
         enemyImage.translate(0,enemyField.getCellSize()*2);
+
+        this.lastDirectionType = DirectionType.DOWN;
     }
 }
